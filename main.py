74a363893c3f744eaa6f6dff87b9e15cbff2d148
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-08-05 15:25:28 -0300

    create database CompanyData

--- main.py
+++ main.py
@@ -13,8 +13,15 @@ def connect_to_mysql():
       print(f"Error connecting to MySQL: {err}")
       return None
 
+def create_database(cursor):
+   cursor.execute("CREATE DATABASE IF NOT EXISTS CompanyData")
+   print('CompanyData database created/exists')
+   
 def run_program():
    conn = connect_to_mysql()
+   if conn:
+      cursor = conn.cursor()
+      create_database(cursor)
 
 if __name__ == '__main__':
    run_program()
