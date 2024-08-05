ba61858c7121010c71231ebc3dbc5b8bf6542b3c
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-08-05 15:23:41 -0300

    insert logic for connect to mysql

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-def creation_of_the_database():
-   print("Creating the database")
+import mysql.connector
+
+def connect_to_mysql():
+   try:
+      conn = mysql.connector.connect(
+         host="localhost",
+         user="root",
+         password=""
+      )
+      print("MySQL connection successful")
+      return conn
+   except mysql.connector.Error as err:
+      print(f"Error connecting to MySQL: {err}")
+      return None
 
 def run_program():
-   creation_of_the_database()
-
+   conn = connect_to_mysql()
 
 if __name__ == '__main__':
    run_program()
